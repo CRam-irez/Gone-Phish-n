716df1e250cbaddd679623a9e06c9a03bbae803b
Christian Ramirez <christian.ramirezz@yahoo.com>
2025-11-08 13:19:03 -0800

    made adjustments to characters that could appear in legit urls

--- src/preprocess_url.py
+++ src/preprocess_url.py
@@ -3,8 +3,8 @@ def preprocess_url(url):
     
     url = url.lower()
     url = re.sub(r"https?://", "", url)
-    url = re.sub(r"www.\.", "", url)
-    url = re.sub(r"[^a-z0-9./?=&-_]", "" ,url)
+    url = re.sub(r"www\.", "", url)
+    url = re.sub(r"[^a-z0-9:/?=&%._\-]", "" ,url)
     tokens = re.split(r"[./?=&-_]+", url)
     tokens = [t for t in tokens if len(t) > 2]
     return tokens
