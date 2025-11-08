cd5f620ef523154dbd440032f00343d8c5e72aaa
Christian Ramirez <christian.ramirezz@yahoo.com>
2025-11-08 12:19:52 -0800

    added URL preprocessing for spam detection

---
+++ src/preprocess_url.py
@@ -0,0 +1,10 @@
+import re
+def preprocess_url(url):
+    
+    url = url.lower()
+    url = re.sub(r"https?://", "", url)
+    url = re.sub(r"www.\.", "", url)
+    url = re.sub(r"[^a-z0-9./?=&-_]", "" ,url)
+    tokens = re.split(r"[./?=&-_]+", url)
+    tokens = [t for t in tokens if len(t) > 2]
+    return tokens
