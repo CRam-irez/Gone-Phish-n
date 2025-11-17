1ea20d935ad2bee4daab6b6784f98688a5038dc1
Christian Ramirez <christian.ramirezz@yahoo.com>
2025-11-17 08:25:32 -0800

    reverted changes, this should work with new dataset

--- src/preprocess_url.py
+++ src/preprocess_url.py
@@ -3,18 +3,9 @@ import re
 def preprocess_url(url):
     
     url = url.lower()
-    parsed = urlparse(url)
-    domain = parsed.netloc
-    path = parsed.path
-    query = parsed.query
-    
-    # Split subdomains
-    domain_tokens = re.split(r"[.]", domain)
-    
-    # Tokenize path + query 
-    path_tokens = re.split(r"[\/\-_?=&]", path + " " + query)
-    
-    # Remove empty
-    tokens = [t for t in domain_tokens + path_tokens if len(t) > 2]
-
+    url = re.sub(r"https?://", "", url)
+    url = re.sub(r"www\.", "", url)
+    url = re.sub(r"[^a-z0-9:/?=&%._\-]", "" ,url)
+    tokens = re.split(r"[./?=&-_]+", url)
+    tokens = [t for t in tokens if len(t) > 2]
     return tokens
