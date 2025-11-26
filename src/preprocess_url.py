f6e022eb153f57440ec2da48eca1d3639fd25a33
Christian Ramirez <christian.ramirezz@yahoo.com>
2025-11-25 22:34:57 -0800

    seperate and tokenize URLS to use for spam detection

--- src/preprocess_url.py
+++ src/preprocess_url.py
@@ -1,11 +1,13 @@
 import re
 
+# Convert a raw URL into a list of tokens used for spam detection
 def preprocess_url(url):
     
     url = url.lower()
     url = re.sub(r"https?://", "", url)
     url = re.sub(r"www\.", "", url)
-    url = re.sub(r"[^a-z0-9:/?=&%._\-]", "" ,url)
-    tokens = re.split(r"[./?=&-_]+", url)
+    url = re.sub(r"[^a-z0-9:/?=&%._\-]", "", url)
+
+    tokens = re.split(r"[./?=&\-_]+", url)
     tokens = [t for t in tokens if len(t) > 2]
     return tokens
